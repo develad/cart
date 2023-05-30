169e37b13abd2d6b2aeb717e8a67a2b2e8ca2360
develad <develadb@gmail.com>
2023-05-30 22:16:38 +0300

    :tada: Style change

--- src/components/EditBtn.tsx
+++ src/components/EditBtn.tsx
@@ -11,8 +11,8 @@ const EditBtn = ({ handleEditList, isLightMode, isEditing }: EditProps) => {
   return (
     <div
       onClick={() => handleEditList()}
-      className={` w-[64px] h-[64px] rounded-2xl border-4 bg-[#5ac65e] grid place-items-center fixed bottom-[150px] left-4 md:left-44 lg:left-[35rem] ${
-        isEditing ? 'bg-green-400 ' : 'bg-orange-400'
+      className={` w-[64px] h-[64px] rounded-2xl border-4 grid place-items-center fixed bottom-[150px] left-4 md:left-44 lg:left-[35rem] ${
+        isEditing ? 'bg-violet-700 ' : 'bg-orange-400'
       } rounded-2xl p-2 border-4 ${
         isLightMode ? 'border-gray-950' : 'border-white'
       } hover:cursor-pointer z-10`}
